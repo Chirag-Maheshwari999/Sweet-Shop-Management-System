e460f1e8243466ed21e2401bd9ce6e183dffb39a
Chirag-Maheshwari999 <chiragmaheshwari037@gmail.com>
2025-12-14 01:54:43 +0530

    end point updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://sweet-shop-management-system-exg4.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
